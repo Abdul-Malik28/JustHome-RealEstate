675bd7f48097dc09425a0f2878af80ab77f839c9
Abdul_Malik <malik.info28@gmail.com>
2024-10-11 18:25:12 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    screens: {
+      sm: '576px',
+      md: '768px',
+      lg: '992px',
+      xl: '1200px',
+      xxl: '1400px',
+    },
+    extend: {
+      colors: {
+        softYellow: 'hsl(44, 70.7%, 67.8%)',
+        softGreen: 'hsl(167, 40%, 21%)',
+        softButter: 'hsl(13, 100.0%, 698.2%)',
+      },
+      fontFamily: {
+        sans: ['Roboto', 'sans-serif']
+      },
+    },
+  },
+  plugins: [],
+}
+
