0fba1804eb10b88a6157e1674a15b509b00ab547
Abdul_Malik <malik.info28@gmail.com>
2024-10-12 21:41:42 +0530

    added properties section

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,18 +14,16 @@ module.exports = {
         softYellow: 'hsl(44, 70.7%, 67.8%)',
         softGreen: 'hsl(167, 40%, 21%)',
         softButter: 'hsl(7, 100.0%, 98.4%)',
+        softGray: 'hsl(0, 0.0%, 96.9%)',
       },
       fontFamily: {
         sans: ['Roboto', 'sans-serif']
       },
+      height: {
+        90: '22rem',
+      },
       backgroundImage: () => ({
         city: "url(../Images/nav-background.jpg)",
-        box1: "url(../Images/box-1-image.jpg)",
-        box2: "url(../Images/box-2-image.jpg)",
-        box3: "url(../Images/box-3-image.jpg)",
-        box4: "url(../Images/box-4-image.jpg)",
-        box5: "url(../Images/box-5-image.jpg)",
-        box6: "url(../Images/box-6-image.jpg)",
       }),
     },
   },
