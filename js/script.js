30209a7806ff8e367a306178216bd04e9319477b
Abdul_Malik <malik.info28@gmail.com>
2024-10-12 19:17:30 +0530

    added features section

--- js/script.js
+++ js/script.js
@@ -5,10 +5,7 @@ const menu = document.getElementById('menu');
 btn.addEventListener('click', navToggle)
 btn2.addEventListener('click', navToggle)
 
-linkForm.addEventListener('submit', formSubmit);
-
 //toggle mobile menu
-
 function navToggle() {
     btn2.classList.toggle('flex');
     btn2.classList.toggle('hidden');
@@ -18,3 +15,16 @@ function navToggle() {
     menu.classList.toggle('flex');
     menu.classList.toggle('hidden');
 }
+
+
+// butter buttons
+const softButterButtons = document.querySelectorAll('.btn-softButter');
+
+softButterButtons.forEach(button => {
+    button.addEventListener('click', function() {
+        
+        softButterButtons.forEach(btn => btn.classList.remove('active'));
+
+        this.classList.add('active');
+    });
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,13 +13,19 @@ module.exports = {
       colors: {
         softYellow: 'hsl(44, 70.7%, 67.8%)',
         softGreen: 'hsl(167, 40%, 21%)',
-        softButter: 'hsl(13, 100.0%, 698.2%)',
+        softButter: 'hsl(7, 100.0%, 98.4%)',
       },
       fontFamily: {
         sans: ['Roboto', 'sans-serif']
       },
       backgroundImage: () => ({
         city: "url(../Images/nav-background.jpg)",
+        box1: "url(../Images/box-1-image.jpg)",
+        box2: "url(../Images/box-2-image.jpg)",
+        box3: "url(../Images/box-3-image.jpg)",
+        box4: "url(../Images/box-4-image.jpg)",
+        box5: "url(../Images/box-5-image.jpg)",
+        box6: "url(../Images/box-6-image.jpg)",
       }),
     },
   },
