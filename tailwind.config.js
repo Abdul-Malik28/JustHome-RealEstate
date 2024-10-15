0470bae572a9d8ae87c00a13407ce3feded2e4bb
Abdul_Malik <malik.info28@gmail.com>
2024-10-15 23:01:39 +0530

    finalized

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,7 @@ module.exports = {
         softButter: 'hsl(7, 100.0%, 98.4%)',
         softGray: 'hsl(0, 0.0%, 96.9%)',
         softBlack: 'hsl(0, 0%, 10%)',
+        borderGreen: '#2f5850',
       },
       fontFamily: {
         sans: ['Roboto', 'sans-serif']
